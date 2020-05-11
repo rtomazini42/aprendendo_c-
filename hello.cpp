ac25693a239d3723ca5d2ac2f4695fb3e1284d9c
Renan Tomazini <renantomazini@gmail.com>
2020-05-11 14:43:09 -0300

    Aprendendo essa merda
    Sim, eu me odeio. Como percebeu?

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Olá mundo. Eu to aprendendo C++, pq eu perdi o sentido da minha vida" << endl;
+    cout << "Testando mais uma linha" << endl;
+    cout << "ainda hoje: desisitndo de C++" << endl;
     return 0;
 }
