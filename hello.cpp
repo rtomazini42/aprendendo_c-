edcbcdea006357dd607574a65b8dcfe4aec6eb7e
Renan Tomazini <renantomazini@gmail.com>
2020-05-11 16:45:33 -0300

    Adicionando linhas
    Tomara que essa porra sirva pelo menos pra consulta no futuro

--- hello.cpp
+++ hello.cpp
@@ -9,5 +9,26 @@ int main()
     cout << "ainda hoje: desisitndo de C++" << endl;
     cout << "numero escondido: "<< numero_teste <<" para dar sorte"<< endl;
 
+    int entrada;
+    cout << "digita algo " <<endl;
+    cin >> entrada;
+    cout << "numero digitado: "<< entrada <<endl;
+
+    //vamos testar ifs e elses
+
+    int numero_secreto = 31;
+    int chute;
+    cout <<"Chuta um numero: " <<endl;
+    cin >> chute;
+    if(chute == numero_secreto){
+        cout <<"Voce acertou!" <<endl;
+    }
+    else if(chute > numero_secreto){
+        cout <<"Numero eh menor que o chute" <<endl;
+
+    }
+    else {
+        cout <<"Numero eh maior que o chute" <<endl;
+    }
     return 0;
 }
