06d4f372032f178ed7029879ed5aa14e6ffb20a7
Renan Tomazini <renantomazini@gmail.com>
2020-05-11 14:59:07 -0300

    Adicionando 1 linhazinha inocente
    Que fara toda essa merda explodir

--- hello.cpp
+++ hello.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main()
-{
+{   int numero_teste = 13;
     cout << "Olá mundo. Eu to aprendendo C++, pq eu perdi o sentido da minha vida" << endl;
     cout << "Testando mais uma linha" << endl;
     cout << "ainda hoje: desisitndo de C++" << endl;
+    cout << "numero escondido: "<< numero_teste <<" para dar sorte"<< endl;
+
     return 0;
 }
