fc3479d26f833b3420fbf308024aaab62c2499d4
Renan Tomazini <renantomazini@gmail.com>
2020-06-09 21:57:51 -0300

    C++ É do caralho
    É tanta blibioteca nessa merda que eu me sinto um livreiro

--- forca.cpp
+++ forca.cpp
@@ -2,15 +2,48 @@
 #include <string>
 #include<map>
 #include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+
 
 using namespace std;
 // make CXXFLAGS= -std=c++11
 
 
-string palavra = "CHINELO";
+string palavra = "___________";
 map <char, bool> chutou;
 vector <char> chutes_errados;
 //vector <char> chutes_errados;
+
+vector <string> le_arquivo(){
+  ifstream arquivo;
+  arquivo.open("bancopalavras.txt");
+  int quantidade_palavras;
+  arquivo>> quantidade_palavras;
+
+  vector<string> palavras_arquivo;
+
+  for(int i = 0; i <quantidade_palavras; i++){
+    string palavra_lida;
+    arquivo >> palavra_lida;
+    palavras_arquivo.push_back(palavra_lida);
+    //cout<<palavras_arquivo.size()<<endl;
+
+  }
+  return palavras_arquivo;
+
+}
+
+void sorteia_palavra(){
+  vector<string> palavras = le_arquivo();
+  srand(time(NULL));
+  //cout <<palavras.size()<< '\n';
+  int indice_sorteado = rand()%palavras.size();
+  //cout <<indice_sorteado<< '\n';
+  //palavra =  palavras[indice_sorteado];
+  cout <<palavra<< '\n';
+}
 
 bool letra(char chute){
 //  for(int i = 0; i < palavra.size(); i++){
@@ -25,6 +58,7 @@ bool letra(char chute){
 
 
 string printar(){
+
   for(char letra:palavra){
     if(chutou [letra]){
       cout << letra << ' ';
@@ -63,6 +97,7 @@ int abertura(){
 
 int gameloop(){
   char chute;
+  //sorteia_palavra();
 
   while (nao_enforcou() && nao_acertou()){
     cout <<"Chute: "<<endl;
@@ -87,9 +122,11 @@ int gameloop(){
   }
 }
 
+
+
 int main () {
   abertura();
-
+  sorteia_palavra();
   printar();
 
   gameloop();
